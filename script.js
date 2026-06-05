9c0172d645d97c72ff5a5d801d9ea72c736ff169
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-06 01:31:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" }
 ];
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement("img");
+
+    img.src = url;
+
+    img.onload = () => resolve(img);
+
+    img.onerror = () => reject(`Failed to load image: ${url}`);
+  });
+}
+
+output.textContent = "Loading...";
+
+Promise.all(
+  images.map((image) => downloadImage(image.url))
+)
+  .then((imgs) => {
+    output.innerHTML = "";
+
+    imgs.forEach((img) => {
+      output.appendChild(img);
+    });
+  })
+  .catch((error) => {
+    output.innerHTML = error;
+  });
